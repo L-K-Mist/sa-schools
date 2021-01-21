ed20eba869633cf14fb57452b12a68d22ee474d1
l-k-mist <dylan@vandenbosch.co.za>
2021-01-21 21:54:57 +0200

    Tried different version of leaflet-defaulticon-compatibility.css

--- src/components/AllSchoolsMap.vue
+++ src/components/AllSchoolsMap.vue
@@ -81,7 +81,7 @@
 <script>
 import Vue2LeafletMarkerCluster from "vue2-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import {
   LMap,
